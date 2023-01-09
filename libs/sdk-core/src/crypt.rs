1b641a4915559f924172a779cff941c971d36f20
ok300 <106775972+ok300@users.noreply.github.com>
2023-01-09 17:12:19 +0100

    Allow unused decrypt method (9 -> 8 warnings)

--- libs/sdk-core/src/crypt.rs
+++ libs/sdk-core/src/crypt.rs
@@ -7,6 +7,7 @@ pub fn encrypt(key: Vec<u8>, msg: Vec<u8>) -> Result<Vec<u8>> {
     }
 }
 
+#[allow(dead_code)]
 pub fn decrypt(key: Vec<u8>, msg: Vec<u8>) -> Result<Vec<u8>> {
     match ecies::decrypt(key.as_slice(), msg.as_slice()) {
         Ok(res) => Ok(res),
