abe4eba301f99add3c6855bab945e983d5bdbfd5
ok300 <106775972+ok300@users.noreply.github.com>
2023-01-11 12:17:18 +0100

    Fix kts and swift tests

--- libs/sdk-bindings/tests/bindings/test_breez_sdk.kts
+++ libs/sdk-bindings/tests/bindings/test_breez_sdk.kts
@@ -10,7 +10,7 @@ class SDKListener: breez_sdk.EventListener {
 try {
     var seed = breez_sdk.mnemonicToSeed("cruise clever syrup coil cute execute laundry general cover prevent law sheriff");
     var credentials = breez_sdk.recoverNode(breez_sdk.Network.BITCOIN, seed);
-    var sdkServices = breez_sdk.initServices(breez_sdk.default_config(breez_sdk.EnvironmentType.Staging), seed, credentials, SDKListener());
+    var sdkServices = breez_sdk.initServices(breez_sdk.defaultConfig(breez_sdk.EnvironmentType.STAGING), seed, credentials, SDKListener());
     sdkServices.start();
     var nodeInfo = sdkServices.nodeInfo();
     //assert(nodeInfo?.id.equals("0352a918bdba7d7a69893a7d52a449f3e6df8e15a0edcc7fe59060be70d6f416f0"));
