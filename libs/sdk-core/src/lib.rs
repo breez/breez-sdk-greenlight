41226b4a95d8e30199651c6a609fe3a9a16a5454
ok300 <106775972+ok300@users.noreply.github.com>
2023-01-19 13:46:46 +0100

    Update docs: update Bindings section

--- libs/sdk-core/src/lib.rs
+++ libs/sdk-core/src/lib.rs
@@ -122,9 +122,12 @@
 //! in a structured form.
 //!
 //!
-//! ## Bindings and Supported Platforms
+//! ## Bindings
 //!
-//! The library can be built both for Android and iOS.
+//! * Dart
+//! * Swift
+//! * Kotlin
+//! * React-Native: coming soon
 
 mod bridge_generated; /* AUTO INJECTED BY flutter_rust_bridge. This line may not be accurate, and you can change it according to your needs. */
 #[macro_use]
