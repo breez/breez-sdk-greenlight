006a8bfc0d4866e5ddc5588e6201cd678cefe419
Roei Erez <roeierez@gmail.com>
2023-04-17 19:39:25 +0300

    Add csharp test

--- libs/sdk-bindings/tests/test_generated_bindings.rs
+++ libs/sdk-bindings/tests/test_generated_bindings.rs
@@ -1,4 +1,20 @@
+use std::process::Command;
+
 uniffi::build_foreign_language_testcases!(
     "tests/bindings/test_breez_sdk.swift",
     "tests/bindings/test_breez_sdk.kts"
 );
+
+#[test]
+fn test_csharp() {
+    let output = Command::new("dotnet")
+        .arg("run")
+        .arg("--project")
+        .arg("tests/bindings/csharp/sdk-cs-demo.csproj")
+        .output()
+        .expect("failed to execute process");
+    println!("status: {}", output.status);
+    println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
+    println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+    assert!(output.status.success());
+}
