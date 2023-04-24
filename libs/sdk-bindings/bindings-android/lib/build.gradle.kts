0afe536fdd714372a166a1d477dc0072d673963a
cnixbtc <111755602+cnixbtc@users.noreply.github.com>
2023-04-24 20:15:29 +0200

    Remove publishing code

--- libs/sdk-bindings/bindings-android/lib/build.gradle.kts
+++ libs/sdk-bindings/bindings-android/lib/build.gradle.kts
@@ -1,13 +1,6 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
-// library version is defined in gradle.properties
-val libraryVersion: String by project
-
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android") version "1.6.10"
-    id("maven-publish")
-    id("signing")
 }
 
 repositories {
@@ -30,13 +23,6 @@ android {
             proguardFiles(file("proguard-android-optimize.txt"), file("proguard-rules.pro"))
         }
     }
-
-    publishing {
-        singleVariant("release") {
-            withSourcesJar()
-            withJavadocJar()
-        }
-    }
 }
 
 dependencies {
@@ -44,49 +30,4 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7")
     implementation("androidx.appcompat:appcompat:1.4.0")
     implementation("androidx.core:core-ktx:1.7.0")
-}
-
-afterEvaluate {
-    publishing {
-        publications {
-            create<MavenPublication>("maven") {
-                groupId = "technology.breez"
-                artifactId = "bindings-android"
-                version = libraryVersion
-
-                from(components["release"])
-                pom {
-                    name.set("Breez SDK")
-                    description.set("Breez SDK Kotlin language bindings.")
-                    url.set("https://breez.technology")
-                    licenses {
-                        license {
-                            name.set("MIT")
-                            url.set("https://github.com/bitcoindevkit/bdk/blob/master/LICENSE-MIT")
-                        }
-                    }
-                    developers {
-                        developer {
-                            id.set("breez_tech")
-                            name.set("Breez")
-                            email.set("contact@breez.technology")
-                        }
-                    }
-                    scm {
-                        connection.set("scm:git:github.com/breez/breez-sdk.git")
-                        developerConnection.set("scm:git:ssh://github.com/breez/breez-sdk.git")
-                        url.set("https://github.com/breez/breez-sdk/tree/main")
-                    }
-                }
-            }
-        }
-    }
-}
-
-signing {
-    val signingKeyId: String? by project
-    val signingKey: String? by project
-    val signingPassword: String? by project
-    useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
-    sign(publishing.publications)
 }
