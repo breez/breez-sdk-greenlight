621c7826818c051dd171d5309bb0f72aa137f5d2
Ross Savage <hello@satimoto.com>
2023-05-23 09:21:53 +0200

    Convert Uint8Array registration credentials to number[] to pass over the bridge, change registerNode to non-optional parameters

--- libs/sdk-react-native/example/utils/storage.ts
+++ libs/sdk-react-native/example/utils/storage.ts
@@ -2,6 +2,14 @@ import SecureStorage from "react-native-secure-storage"
 
 const secureConfig = {}
 
+export const clearSecureStorage = async (): Promise<void> => {
+    const keys = await SecureStorage.getAllKeys()
+
+    for await (const key of keys) {
+        await SecureStorage.removeItem(key, secureConfig)
+    }
+}
+
 export const setSecureItem = async (key: string, item: any): Promise<void> => {
     return await SecureStorage.setItem(key, JSON.stringify(item), secureConfig)
 }
