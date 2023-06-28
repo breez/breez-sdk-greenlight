9e06003f64dd382706635d6dd6223e0da5a5bf6a
cnixbtc <111755602+cnixbtc@users.noreply.github.com>
2023-06-28 21:56:58 +0200

    jitpack

---
+++ libs/sdk-bindings/bindings-android/buildForJitpack.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+if [[ -z "$JITPACK" ]]; then
+    exit 1
+fi
+
+echo "JitPack building version $VERSION."
+cd $(dirname $0)
+./gradlew publishToMavenLocal -PlibraryVersion=$VERSION
