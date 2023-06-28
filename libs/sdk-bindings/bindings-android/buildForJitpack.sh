877ada25db3d9251d26c1df752fe8be1114fbbc5
cnixbtc <111755602+cnixbtc@users.noreply.github.com>
2023-06-28 21:41:18 +0200

    Execute jitpack build via script

---
+++ libs/sdk-bindings/bindings-android/buildForJitpack.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -e
+
+if [[ -z "$JITPACK" ]]; then
+    exit 1
+fi
+
+
+echo "JitPack building version $VERSION."
+cd $(dirname $0)
+./gradlew publishToMavenLocal -PlibraryVersion=$VERSION
