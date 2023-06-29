6d9b7b7479615c92a33e9f02133bfee313972ab0
cnixbtc <111755602+cnixbtc@users.noreply.github.com>
2023-06-29 16:58:53 +0200

    Run rust install non-interactively

--- libs/sdk-bindings/bindings-android/buildForJitpack.sh
+++ libs/sdk-bindings/bindings-android/buildForJitpack.sh
@@ -9,7 +9,7 @@ fi
 echo "JitPack building version $VERSION."
 cd $(dirname $0)
 cd ..
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 make init
 make bindings-android
 cd bindings-android
