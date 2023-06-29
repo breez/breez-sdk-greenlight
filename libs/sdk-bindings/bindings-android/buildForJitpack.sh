228ef10e660e4e6a6a0556c998911c028337bc81
cnixbtc <111755602+cnixbtc@users.noreply.github.com>
2023-06-29 17:13:50 +0200

    Log ndk version

--- libs/sdk-bindings/bindings-android/buildForJitpack.sh
+++ libs/sdk-bindings/bindings-android/buildForJitpack.sh
@@ -11,6 +11,10 @@ cd $(dirname $0)
 cd ..
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source "$HOME/.cargo/env"
+echo $ANDROID_HOME
+echo $NDK_HOME
+echo $ANDROID_NDK_ROOT
+ls $ANDROID_HOME/ndk/
 make init
 make bindings-android
 cd bindings-android
