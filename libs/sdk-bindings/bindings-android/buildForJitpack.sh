0fd04c28c48c15e0da5c2592ac34023680efa708
cnixbtc <111755602+cnixbtc@users.noreply.github.com>
2023-06-29 19:21:43 +0200

    Update build

--- libs/sdk-bindings/bindings-android/buildForJitpack.sh
+++ libs/sdk-bindings/bindings-android/buildForJitpack.sh
@@ -8,21 +8,35 @@ fi
 
 echo "JitPack building version $VERSION."
 cd $(dirname $0)
-cd ..
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-source "$HOME/.cargo/env"
-echo "ls ANDROID_HOME"
-ls $ANDROID_HOME
+
+ls
+curl https://mvn.breez.technology/releases/breez_sdk/bindings-android/0.1.0/bindings-android-0.1.0.aar --output bindings-android-0.1.0.aar
+curl https://mvn.breez.technology/releases/breez_sdk/bindings-android/0.1.0/bindings-android-0.1.0.pom --output bindings-android-0.1.0.pom
+ls
+
 echo "---"
-# yes | $ANDROID_HOME/tools/bin/sdkmanager --install "ndk;23.1.7779620"
+
+ls ~/.m2/repository
+
+mvn install:install-file -Dfile=bindings-android-0.1.0.aar -DpomFile=indings-android-0.1.0.pom
+
+ls ~/.m2/repository
+
+# cd ..
+# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+# source "$HOME/.cargo/env"
+# echo "ls ANDROID_HOME"
+# ls $ANDROID_HOME
+# echo "---"
+# # yes | $ANDROID_HOME/tools/bin/sdkmanager --install "ndk;23.1.7779620"
+# # echo "ls ANDROID_HOME/ndk"
+# # ls $ANDROID_HOME/ndk
+# # echo "---"
 # echo "ls ANDROID_HOME/ndk"
 # ls $ANDROID_HOME/ndk
 # echo "---"
-echo "ls ANDROID_HOME/ndk"
-ls $ANDROID_HOME/ndk
-echo "---"
-export ANDROID_NDK_HOME=$ANDROID_HOME/ndk/23.1.7779620
-make init
-make bindings-android
-cd bindings-android
-./gradlew publishToMavenLocal -PlibraryVersion=$VERSION
+# export ANDROID_NDK_HOME=$ANDROID_HOME/ndk/23.1.7779620
+# make init
+# make bindings-android
+# cd bindings-android
+# ./gradlew publishToMavenLocal -PlibraryVersion=$VERSION
