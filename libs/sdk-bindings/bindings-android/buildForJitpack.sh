92abbc57b78b76af4f37839f041aeee93172cc46
cnixbtc <111755602+cnixbtc@users.noreply.github.com>
2023-06-29 17:34:59 +0200

    Set ndk home

--- libs/sdk-bindings/bindings-android/buildForJitpack.sh
+++ libs/sdk-bindings/bindings-android/buildForJitpack.sh
@@ -11,13 +11,7 @@ cd $(dirname $0)
 cd ..
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source "$HOME/.cargo/env"
-# yes | ${ANDROID_HOME}/tools/bin/sdkmanager --licenses
-# yes | ${ANDROID_HOME}/tools/bin/sdkmanager --install "ndk;23.1.7779620"
-echo $ANDROID_HOME
-echo $NDK_HOME
-echo $ANDROID_NDK_ROOT
-ls $ANDROID_HOME
-ls $ANDROID_HOME/ndk-bundle
+export ANDROID_NDK_HOME=$ANDROID_HOME/ndk-bundle
 make init
 make bindings-android
 cd bindings-android
