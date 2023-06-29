e47fbd80a6127c8ca4cee6b9772210e5d0c3b132
cnixbtc <111755602+cnixbtc@users.noreply.github.com>
2023-06-29 20:44:29 +0200

    Updaet install plugin version

--- libs/sdk-bindings/bindings-android/buildForJitpack.sh
+++ libs/sdk-bindings/bindings-android/buildForJitpack.sh
@@ -14,8 +14,8 @@ curl https://mvn.breez.technology/releases/breez_sdk/bindings-android/0.1.0/bind
 curl https://mvn.breez.technology/releases/breez_sdk/bindings-android/0.1.0/bindings-android-0.1.0-sources.jar --insecure --output bindings-android-0.1.0-sources.jar
 curl https://mvn.breez.technology/releases/breez_sdk/bindings-android/0.1.0/bindings-android-0.1.0.pom --insecure --output bindings-android-0.1.0.pom
 
-mvn install:install-file -Dfile=bindings-android-0.1.0.aar -DpomFile=bindings-android-0.1.0.pom
-mvn install:install-file -Dfile=bindings-android-0.1.0.module -DpomFile=bindings-android-0.1.0.pom
-mvn install:install-file -Dfile=bindings-android-0.1.0-sources.jar -DpomFile=bindings-android-0.1.0.pom
+mvn org.apache.maven.plugins:maven-install-plugin:3.1.1:install-file -Dfile=bindings-android-0.1.0.aar -DpomFile=bindings-android-0.1.0.pom
+mvn org.apache.maven.plugins:maven-install-plugin:3.1.1:install-file -Dfile=bindings-android-0.1.0.module -DpomFile=bindings-android-0.1.0.pom
+mvn org.apache.maven.plugins:maven-install-plugin:3.1.1:install-file -Dfile=bindings-android-0.1.0-sources.jar -DpomFile=bindings-android-0.1.0.pom
 
 ls /home/jitpack/.m2/repository/breez_sdk/bindings-android/0.1.0/
