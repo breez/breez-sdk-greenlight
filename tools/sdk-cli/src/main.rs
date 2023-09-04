3f242767c04a92f5238006b43f3266e913ec1e21
Roei Erez <roeierez@gmail.com>
2023-09-04 22:18:12 +0300

    Use data dir for cli logging

--- tools/sdk-cli/src/main.rs
+++ tools/sdk-cli/src/main.rs
@@ -25,8 +25,7 @@ async fn main() {
         return;
     }
 
-    BreezServices::set_log_directory("/Users/roeierez/test/sdk-logs".to_string())
-        .expect("Failed to init logging");
+    BreezServices::set_log_directory(data_dir.clone()).expect("Failed to init logging");
 
     let persistence = CliPersistence { data_dir };
     let history_file = &persistence.history_file();
