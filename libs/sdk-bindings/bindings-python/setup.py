ccbe870aacf88fb66cbc9d4d094c63ca89afa2a2
cnixbtc <111755602+cnixbtc@users.noreply.github.com>
2023-10-27 20:12:41 +0200

    Add CI

--- libs/sdk-bindings/bindings-python/setup.py
+++ libs/sdk-bindings/bindings-python/setup.py
@@ -2,13 +2,30 @@
 
 from setuptools import setup
 
+LONG_DESCRIPTION = """# Breez SDK
+Python language bindings for the [Breez SDK](https://github.com/breez/breez-sdk).
+
+## Installing
+
+```shell
+pip install breez_sdk
+```
+
+## Docs
+
+See [sdk-doc.breez.technology](https://sdk-doc.breez.technology).
+"""
+
 setup(
     name="breez_sdk",
-    version="0.2.7.dev0",
+    version="0.2.7.dev9",
     description="Python language bindings for the Breez SDK",
-    include_package_data=True,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
     packages=["breez_sdk"],
     package_dir={"breez_sdk": "./src/breez_sdk"},
+    include_package_data=True,
+    package_data={"breez_sdk": ["*.dylib"]},
     url="https://github.com/breez/breez-sdk",
     author="Breez <contact@breez.technology>",
     license="MIT",
