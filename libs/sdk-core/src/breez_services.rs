712202e5486c7814b2a5516f542445bcb08ae42e
ok300 <106775972+ok300@users.noreply.github.com>
2024-02-19 10:23:50 +0100

    Expose redeem_swap as SDK method

--- libs/sdk-core/src/breez_services.rs
+++ libs/sdk-core/src/breez_services.rs
@@ -767,6 +767,11 @@ impl BreezServices {
         Ok(())
     }
 
+    pub async fn redeem_swap(&self, swap_address: String) -> SdkResult<()> {
+        self.btc_receive_swapper.redeem_swap(swap_address).await?;
+        Ok(())
+    }
+
     /// Lookup the reverse swap fees (see [ReverseSwapServiceAPI::fetch_reverse_swap_fees]).
     ///
     /// To get the total estimated fees for a specific amount, specify the amount to be sent in
