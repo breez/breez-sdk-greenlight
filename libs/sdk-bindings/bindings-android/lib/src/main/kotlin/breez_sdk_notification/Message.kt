882b4423b95372b688baac85247bdd0ba7f37fb2
Ross Savage <hello@satimoto.com>
2024-02-21 12:05:58 +0100

    Remove message priority checking

--- libs/sdk-bindings/bindings-android/lib/src/main/kotlin/breez_sdk_notification/Message.kt
+++ libs/sdk-bindings/bindings-android/lib/src/main/kotlin/breez_sdk_notification/Message.kt
@@ -3,24 +3,19 @@ package breez_sdk_notification
 import android.os.Parcel
 import android.os.Parcelable
 
-data class Message(val priority: Int, val type: String?, val payload: String?): Parcelable {
-    constructor(parcel: Parcel) : this(parcel.readInt(), parcel.readString(), parcel.readString())
+data class Message(val type: String?, val payload: String?): Parcelable {
+    constructor(parcel: Parcel) : this(parcel.readString(), parcel.readString())
 
     override fun describeContents(): Int {
         return 0
     }
 
     override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeInt(priority)
         parcel.writeString(type)
         parcel.writeString(payload)
     }
 
     companion object CREATOR : Parcelable.Creator<Message> {
-        val PRIORITY_UNKNOWN = 0
-        val PRIORITY_HIGH = 1
-        val PRIORITY_NORMAL = 2
-
         override fun createFromParcel(parcel: Parcel): Message {
             return Message(parcel)
         }
