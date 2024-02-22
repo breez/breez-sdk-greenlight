513aa267efa4cf276364e3c21ed3b5460eac3c74
Ross Savage <hello@satimoto.com>
2024-02-22 12:56:18 +0100

    Move into Message creating the Message from an Intent

--- libs/sdk-bindings/bindings-android/lib/src/main/kotlin/breez_sdk_notification/Message.kt
+++ libs/sdk-bindings/bindings-android/lib/src/main/kotlin/breez_sdk_notification/Message.kt
@@ -1,9 +1,11 @@
 package breez_sdk_notification
 
+import android.content.Intent
+import android.os.Build
 import android.os.Parcel
 import android.os.Parcelable
 
-data class Message(val type: String?, val payload: String?): Parcelable {
+data class Message(val type: String?, val payload: String?) : Parcelable {
     constructor(parcel: Parcel) : this(parcel.readString(), parcel.readString())
 
     override fun describeContents(): Int {
@@ -16,6 +18,16 @@ data class Message(val type: String?, val payload: String?): Parcelable {
     }
 
     companion object CREATOR : Parcelable.Creator<Message> {
+        @Suppress("DEPRECATION")
+        fun createFromIntent(intent: Intent?): Message? {
+            return intent?.let {
+                return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) it.getParcelableExtra(
+                    Constants.EXTRA_REMOTE_MESSAGE,
+                    Message::class.java
+                ) else it.getParcelableExtra(Constants.EXTRA_REMOTE_MESSAGE)
+            }
+        }
+
         override fun createFromParcel(parcel: Parcel): Message {
             return Message(parcel)
         }
