97d2338860f42979c8c8e9b2e3bb2de600342200
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2024-02-29 12:37:44 +0300

    LogHelper now takes logging directory as argument
    Previous implementation would only work on Flutter applications and using applicationContext.dataDir/filesDir wouldn't be applicable to most apps.

--- libs/sdk-bindings/bindings-android/lib/src/main/kotlin/breez_sdk_notification/LogHelper.kt
+++ libs/sdk-bindings/bindings-android/lib/src/main/kotlin/breez_sdk_notification/LogHelper.kt
@@ -1,7 +1,5 @@
 package breez_sdk_notification
 
-import android.content.Context
-import io.flutter.util.PathUtils
 import org.tinylog.kotlin.Logger
 import java.io.File
 
@@ -13,15 +11,9 @@ class LogHelper {
         private var isInit: Boolean? = null
 
         fun configureLogger(
-            applicationContext: Context,
-            relativeLogDirectory: String = "/logs/"
+            loggingDir: File,
         ): Boolean? {
             synchronized(this) {
-                val loggingDir =
-                    File(PathUtils.getDataDirectory(applicationContext), relativeLogDirectory).apply {
-                        mkdirs()
-                    }
-
                 System.setProperty("tinylog.directory", loggingDir.absolutePath)
                 System.setProperty("tinylog.timestamp", System.currentTimeMillis().toString())
 
