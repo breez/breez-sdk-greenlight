94772ef60f199790d25f00344b8e60b724e95eca
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2024-03-06 19:52:56 +0300

    Re-add SdkLogInitializer & SdkLogListener to Flutter plugin

---
+++ libs/sdk-flutter/android/src/main/kotlin/com/breez/breez_sdk/SdkLogListener.kt
@@ -0,0 +1,31 @@
+package breez_sdk_notification
+
+import breez_sdk.LogEntry
+import breez_sdk.LogStream
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+
+class SdkLogListener : LogStream {
+    private val scope = CoroutineScope(SupervisorJob())
+
+    private val _logEvents = MutableSharedFlow<LogEntry>()
+    private val logEvents: SharedFlow<LogEntry> = _logEvents.asSharedFlow()
+
+    override fun log(l: LogEntry) {
+        scope.launch {
+            _logEvents.emit(l)
+        }
+    }
+
+    fun subscribe(scope: CoroutineScope, block: suspend (LogEntry) -> Unit) =
+        logEvents.onEach(block).launchIn(scope)
+
+    fun unsubscribe(scope: CoroutineScope) = scope.cancel()
+}
