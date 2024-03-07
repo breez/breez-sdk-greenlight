4b9c355708bbc55aea96cb7e14d927162891d906
ok300 <106775972+ok300@users.noreply.github.com>
2024-03-07 12:27:46 +0100

    Fix dart bridge return type

--- libs/sdk-flutter/lib/breez_sdk.dart
+++ libs/sdk-flutter/lib/breez_sdk.dart
@@ -339,7 +339,7 @@ class BreezSDK {
     return await _lnToolkit.sendOnchain(req: req);
   }
 
-  Future<PayOnchainResponse> onchainPaymentLimits() async {
+  Future<OnchainPaymentLimitsResponse> onchainPaymentLimits() async {
     return await _lnToolkit.onchainPaymentLimits();
   }
 
