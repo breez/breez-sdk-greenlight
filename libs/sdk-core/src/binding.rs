b5e8ba4c3cdbf6b1349908a55eed838ea72fa77f
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2024-03-20 00:59:29 +0300

    Move loggers under `logger.rs`
    Dart, UniFFI & GlobalSDK logger's are moved under logger.rs
    
    .

--- libs/sdk-core/src/binding.rs
+++ libs/sdk-core/src/binding.rs
@@ -15,7 +15,6 @@ use std::sync::Arc;
 
 use anyhow::{anyhow, Result};
 use flutter_rust_bridge::StreamSink;
-use log::{Level, LevelFilter, Metadata, Record};
 use once_cell::sync::{Lazy, OnceCell};
 use sdk_common::invoice;
 pub use sdk_common::prelude::{
@@ -34,6 +33,11 @@ use crate::error::{
     ConnectError, ReceiveOnchainError, ReceivePaymentError, RedeemOnchainError, SdkError,
     SendOnchainError, SendPaymentError,
 };
+use crate::fiat::{FiatCurrency, Rate};
+use crate::input_parser::{self, InputType, LnUrlAuthRequestData};
+use crate::invoice::{self, LNInvoice};
+use crate::lnurl::pay::model::LnUrlPayResult;
+use crate::logger::{init_dart_logger, DartLogger};
 use crate::lsp::LspInformation;
 use crate::models::{Config, LogEntry, NodeState, Payment, SwapInfo};
 use crate::{
@@ -295,7 +299,6 @@ static BREEZ_SERVICES_INSTANCE: Lazy<Mutex<Option<Arc<BreezServices>>>> =
     Lazy::new(|| Mutex::new(None));
 static NOTIFICATION_STREAM: OnceCell<StreamSink<BreezEvent>> = OnceCell::new();
 static RT: Lazy<tokio::runtime::Runtime> = Lazy::new(|| tokio::runtime::Runtime::new().unwrap());
-static LOG_INIT: OnceCell<bool> = OnceCell::new();
 
 /*  Breez Services API's */
 
@@ -416,10 +419,8 @@ pub fn breez_events_stream(s: StreamSink<BreezEvent>) -> Result<()> {
 
 /// If used, this must be called before `connect`. It can only be called once.
 pub fn breez_log_stream(s: StreamSink<LogEntry>) -> Result<()> {
-    LOG_INIT
-        .set(true)
-        .map_err(|_| anyhow!("Log stream already created"))?;
-    BindingLogger::init(s);
+    init_dart_logger();
+    DartLogger::set_stream_sink(s);
     Ok(())
 }
 
@@ -799,34 +800,6 @@ impl EventListener for BindingEventListener {
             stream.add(e);
         }
     }
-}
-
-struct BindingLogger {
-    log_stream: StreamSink<LogEntry>,
-}
-
-impl BindingLogger {
-    fn init(log_stream: StreamSink<LogEntry>) {
-        let binding_logger = BindingLogger { log_stream };
-        log::set_boxed_logger(Box::new(binding_logger)).unwrap();
-        log::set_max_level(LevelFilter::Trace);
-    }
-}
-
-impl log::Log for BindingLogger {
-    fn enabled(&self, m: &Metadata) -> bool {
-        m.level() <= Level::Trace
-    }
-
-    fn log(&self, record: &Record) {
-        if self.enabled(record.metadata()) {
-            self.log_stream.add(LogEntry {
-                line: record.args().to_string(),
-                level: record.level().as_str().to_string(),
-            });
-        }
-    }
-    fn flush(&self) {}
 }
 
 async fn get_breez_services() -> Result<Arc<BreezServices>, SdkError> {
