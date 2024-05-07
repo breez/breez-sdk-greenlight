589d12bc03501d5a67a354fea439fe9ca539471c
Roei Erez <roeierez@gmail.com>
2024-05-07 21:03:35 +0300

    fix build on macos
    (cherry picked from commit 9142b3c21973a4cecccb1b7d9b124daa6b1570a8)

--- libs/sdk-bindings/build.rs
+++ libs/sdk-bindings/build.rs
@@ -20,17 +20,19 @@ fn setup_x86_64_android_workaround() {
                 "Unsupported OS. You must use either Linux, MacOS or Windows to build the crate."
             ),
         };
-        let clang_version =
-            env::var("NDK_CLANG_VERSION").unwrap_or_else(|_| DEFAULT_CLANG_VERSION.to_owned());
-        let linux_x86_64_lib_dir = format!(
-            "toolchains/llvm/prebuilt/{build_os}-x86_64/lib64/clang/{clang_version}/lib/linux/"
-        );
-        let linkpath = format!("{android_ndk_home}/{linux_x86_64_lib_dir}");
-        if Path::new(&linkpath).exists() {
-            println!("cargo:rustc-link-search={android_ndk_home}/{linux_x86_64_lib_dir}");
-            println!("cargo:rustc-link-lib=static=clang_rt.builtins-x86_64-android");
-        } else {
-            panic!("Path {linkpath} not exists");
+        if build_os == "linux" {
+            let clang_version =
+                env::var("NDK_CLANG_VERSION").unwrap_or_else(|_| DEFAULT_CLANG_VERSION.to_owned());
+            let linux_x86_64_lib_dir = format!(
+                "toolchains/llvm/prebuilt/{build_os}-x86_64/lib64/clang/{clang_version}/lib/linux/"
+            );
+            let linkpath = format!("{android_ndk_home}/{linux_x86_64_lib_dir}");
+            if Path::new(&linkpath).exists() {
+                println!("cargo:rustc-link-search={android_ndk_home}/{linux_x86_64_lib_dir}");
+                println!("cargo:rustc-link-lib=static=clang_rt.builtins-x86_64-android");
+            } else {
+                panic!("Path {linkpath} not exists");
+            }
         }
     }
 }
