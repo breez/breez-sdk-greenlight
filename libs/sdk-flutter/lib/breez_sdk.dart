c9ff3307534e901a2511438af0ffd5983253a112
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2024-05-28 12:35:10 +0300

    Apply BigInt changes

--- libs/sdk-flutter/lib/breez_sdk.dart
+++ libs/sdk-flutter/lib/breez_sdk.dart
@@ -593,7 +593,7 @@ extension SDKConfig on Config {
     String? defaultLspId,
     String? apiKey,
     double? maxfeePercent,
-    int? exemptfeeMsat,
+    BigInt? exemptfeeMsat,
     NodeConfig? nodeConfig,
   }) {
     return Config(
