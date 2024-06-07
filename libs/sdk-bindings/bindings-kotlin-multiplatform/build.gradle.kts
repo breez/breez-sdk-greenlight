050194c076f8aab1995dad9a45c17b1adacde034
Angelos Veglektsis <angelos@aveworks.com>
2024-06-07 12:09:07 +0300

    Update bindings-kotlin-multiplatform to use version catalog Update dependencies

--- libs/sdk-bindings/bindings-kotlin-multiplatform/build.gradle.kts
+++ libs/sdk-bindings/bindings-kotlin-multiplatform/build.gradle.kts
@@ -1,17 +1,21 @@
 
 buildscript {
+    repositories {
+        mavenCentral()
+    }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.1.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.21")
-        classpath("org.jetbrains.kotlinx:atomicfu-gradle-plugin:0.23.1")
+        classpath(libs.android.gradle.plugin)
+        classpath(libs.kotlin.gradle.plugin)
+        classpath(libs.atomicfu)
     }
 }
 
 plugins {
     //trick: for the same plugin versions in all sub-modules
-    id("com.android.library").version("8.1.0").apply(false)
-    kotlin("multiplatform").version("1.9.21").apply(false)
+    alias(libs.plugins.androidLibrary) apply false
+    alias(libs.plugins.kotlinMultiplatform) apply false
 }
+
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
