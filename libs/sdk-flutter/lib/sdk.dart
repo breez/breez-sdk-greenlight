e4af55c0d1b813003dcd6eda0a9aef9e5fcc438f
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2024-06-24 14:58:29 +0300

    Do not export "frb_generated.io.dart"

--- libs/sdk-flutter/lib/sdk.dart
+++ libs/sdk-flutter/lib/sdk.dart
@@ -2,7 +2,6 @@ export 'generated/binding.dart';
 export 'generated/breez_services.dart';
 export 'generated/chain.dart';
 export 'generated/frb_generated.dart';
-export 'generated/frb_generated.io.dart';
 export 'generated/lsp.dart';
 export 'generated/lnurl/pay.dart';
 export 'generated/models.dart';
