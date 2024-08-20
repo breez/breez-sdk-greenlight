14715a7a178de28171de8d75f23b5d39a963fdb1
ok300 <106775972+ok300@users.noreply.github.com>
2024-08-20 15:18:17 +0000

    sdk-common: simplify prelude (#1078)

--- libs/sdk-common/src/lib.rs
+++ libs/sdk-common/src/lib.rs
@@ -31,6 +31,8 @@ pub mod prelude {
     pub use crate::buy::moonpay::*;
     pub use crate::input_parser::*;
     pub use crate::invoice::*;
+    #[cfg(feature = "liquid")]
+    pub use crate::liquid::*;
     pub use crate::lnurl::error::*;
     pub use crate::lnurl::model::*;
     pub use crate::lnurl::specs::auth::model::*;
