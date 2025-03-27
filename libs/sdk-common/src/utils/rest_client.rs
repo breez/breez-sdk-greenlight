cc331f34102abda3a39a51524307688a74458050
Daniel Granhão <32176319+danielgranhao@users.noreply.github.com>
2025-03-27 22:25:11 +0000

    sdk-common: drop Send and Sync on Wasm (#1189)
    * sdk-common: use Rc instead of Arc on Wasm
    
    * sdk-common: use MaybeSend and MaybeSync on traits

--- libs/sdk-common/src/utils/rest_client.rs
+++ libs/sdk-common/src/utils/rest_client.rs
@@ -1,13 +1,13 @@
+use crate::error::{ServiceConnectivityError, ServiceConnectivityErrorKind};
 use log::*;
+use maybe_sync::{MaybeSend, MaybeSync};
 use reqwest::Client;
 use std::{collections::HashMap, time::Duration};
-
-use crate::error::{ServiceConnectivityError, ServiceConnectivityErrorKind};
 
 const REQUEST_TIMEOUT: Duration = Duration::from_secs(30);
 
 #[sdk_macros::async_trait]
-pub trait RestClient: Send + Sync {
+pub trait RestClient: MaybeSend + MaybeSync {
     /// Makes a GET request and logs on DEBUG.
     /// ### Arguments
     /// - `url`: the URL on which GET will be called
