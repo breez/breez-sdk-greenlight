7337c823e39f317e48f72a217637e1f1e2960aba
Ross Savage <mr.dangeross@googlemail.com>
2025-05-23 16:34:52 +0200

    Update Kotlin Multiplatform bindgen to uniffi 0.28

--- libs/sdk-react-native/example/babel.config.js
+++ libs/sdk-react-native/example/babel.config.js
@@ -5,7 +5,6 @@ module.exports = {
             "module-resolver",
             {
                 alias: {
-                    crypto: "react-native-quick-crypto",
                     stream: "stream-browserify",
                     buffer: "@craftzdog/react-native-buffer"
                 }
