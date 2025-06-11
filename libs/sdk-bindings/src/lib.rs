87205a687b9931d7a71f024d35c8c8ccefd10229
Ross Savage <551697+dangeross@users.noreply.github.com>
2025-06-11 17:53:07 +0000

    Update Kotlin Multiplatform bindgen to uniffi 0.28 (#1219)
    (cherry picked from commit 37446b8f3d2ea8b5d3b72e0726b6c332d91cd941)

--- libs/sdk-bindings/src/lib.rs
+++ libs/sdk-bindings/src/lib.rs
@@ -1,9 +1,3 @@
-//! Uniffi bindings
-#[cfg(feature = "uniffi-25")]
-extern crate uniffi_25 as uniffi;
-#[cfg(feature = "uniffi-28")]
-extern crate uniffi_28 as uniffi;
-
 use std::sync::Arc;
 
 use anyhow::Result;
