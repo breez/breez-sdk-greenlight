3eec610e87c3df5018ff59fc20098c333ebb5487
Andrei <andrei.i@posteo.de>
2025-09-15 00:00:00 +0000

    Implement `LspAPI` and `Receiver` for LDK Node

--- libs/sdk-core/src/ldk/event_handling.rs
+++ libs/sdk-core/src/ldk/event_handling.rs
@@ -1,14 +1,14 @@
 use std::sync::Arc;
 
-use ldk_node::lightning::ln::channelmanager::PaymentId;
-use ldk_node::payment::PaymentKind;
 use ldk_node::{Event, Node};
 use tokio::sync::{broadcast, mpsc};
 
+use crate::ldk::node_api::PreimageStore;
 use crate::node_api::IncomingPayment;
 
 pub async fn start_event_handling(
     node: Arc<Node>,
+    preimages: PreimageStore,
     incoming_payments_tx: broadcast::Sender<IncomingPayment>,
     mut shutdown: mpsc::Receiver<()>,
 ) {
@@ -23,22 +23,60 @@ pub async fn start_event_handling(
         debug!("Event: {event:?}");
 
         match event {
-            Event::PaymentReceived { payment_id, .. } => {
-                match handle_received_payment(&node, payment_id) {
-                    Ok(Some(payment)) => {
+            Event::PaymentReceived {
+                payment_id,
+                payment_hash,
+                amount_msat,
+                ..
+            } => {
+                let preimage = preimages.lock().unwrap().remove(&payment_hash);
+                match preimage {
+                    Some(preimage) => {
+                        // TODO: Load bolt11 from the store.
+                        let bolt11 = String::new();
+                        let payment = IncomingPayment {
+                            label: String::new(),
+                            payment_hash: payment_hash.0.to_vec(),
+                            preimage: preimage.0.to_vec(),
+                            amount_msat,
+                            bolt11,
+                        };
                         if let Err(e) = incoming_payments_tx.send(payment) {
                             warn!("Failed to send payment to incoming_payments_tx: {e}");
                         }
                     }
-                    Ok(None) => (),
-                    Err(e) => {
-                        error!("Failed to handle PaymentReceived event for payment with id={payment_id:?}: {e}")
+                    None => {
+                        error!("Payment received but preimage not found for payment with id={payment_id:?}");
                     }
-                };
+                }
             }
             Event::PaymentSuccessful { .. } => (),
             Event::PaymentFailed { .. } => (),
-            Event::PaymentClaimable { .. } => (),
+            Event::PaymentClaimable {
+                payment_id,
+                payment_hash,
+                claimable_amount_msat,
+                ..
+            } => {
+                let preimage = preimages.lock().unwrap().get(&payment_hash).cloned();
+                match preimage {
+                    Some(preimage) => {
+                        if let Err(e) = node.bolt11_payment().claim_for_hash(
+                            payment_hash,
+                            claimable_amount_msat,
+                            preimage,
+                        ) {
+                            error!("Failed to claim payment: {e}");
+                        }
+                    }
+                    None => {
+                        error!("Payment claimable but preimage not found for payment with id={payment_id:?}");
+                        if let Err(e) = node.bolt11_payment().fail_for_hash(payment_hash) {
+                            error!("Failed to fail payment: {e}");
+                        }
+                    }
+                }
+            }
             Event::PaymentForwarded { .. } => (),
             Event::ChannelPending { .. } => (),
             Event::ChannelReady { .. } => (),
@@ -49,32 +87,4 @@ pub async fn start_event_handling(
             error!("Failed to report that event was handled: {e}");
         }
     }
-}
-
-fn handle_received_payment(
-    node: &Node,
-    payment_id: Option<PaymentId>,
-) -> Result<Option<IncomingPayment>, &'static str> {
-    let payment_details = node
-        .list_payments_with_filter(|p| Some(p.id) == payment_id)
-        .into_iter()
-        .next()
-        .ok_or("Payment not found")?;
-
-    let (hash, preimage) = match payment_details.kind {
-        PaymentKind::Bolt11 { hash, preimage, .. }
-        | PaymentKind::Bolt11Jit { hash, preimage, .. } => (hash, preimage),
-        _ => return Ok(None),
-    };
-    let preimage = preimage.ok_or("Empty preimage")?;
-    let amount_msat = payment_details.amount_msat.ok_or("Empty amount")?;
-    // TODO: Load bolt11 from the store.
-    let bolt11 = String::new();
-    Ok(Some(IncomingPayment {
-        label: String::new(),
-        payment_hash: hash.0.to_vec(),
-        preimage: preimage.0.to_vec(),
-        amount_msat,
-        bolt11,
-    }))
 }
--- libs/sdk-core/src/node_builder.rs
+++ libs/sdk-core/src/node_builder.rs
@@ -1,14 +1,22 @@
 use std::sync::Arc;
 
 use crate::backup::BackupTransport;
+use crate::breez_services::Receiver;
 #[cfg(feature = "greenlight")]
 use crate::greenlight::{GLBackupTransport, Greenlight};
 #[cfg(feature = "ldk")]
 use crate::ldk::{Ldk, LdkBackupTransport};
-use crate::models::Config;
+use crate::models::{Config, LspAPI};
 use crate::node_api::{NodeAPI, NodeResult};
 use crate::persist::db::SqliteStorage;
 use crate::NodeConfig;
+
+pub struct NodeImpls {
+    pub node: Arc<dyn NodeAPI>,
+    pub backup_transport: Arc<dyn BackupTransport>,
+    pub lsp: Option<Arc<dyn LspAPI>>,
+    pub receiver: Option<Arc<dyn Receiver>>,
+}
 
 #[allow(unused_variables)]
 pub async fn build_node(
@@ -16,14 +24,21 @@ pub async fn build_node(
     seed: Vec<u8>,
     restore_only: Option<bool>,
     persister: Arc<SqliteStorage>,
-) -> NodeResult<(Arc<dyn NodeAPI>, Arc<dyn BackupTransport>)> {
+) -> NodeResult<NodeImpls> {
     // TODO: Add NodeConfig::Ldk variant once the feature is developed.
     #[cfg(feature = "ldk")]
     {
         let ldk = Ldk::build(config, &seed, restore_only).await?;
         let ldk = Arc::new(ldk);
         let backup_transport = Arc::new(LdkBackupTransport {});
-        return Ok((ldk, backup_transport));
+        let lsp: Option<Arc<dyn LspAPI>> = Some(ldk.clone());
+        let receiver: Option<Arc<dyn Receiver>> = Some(ldk.clone());
+        return Ok(NodeImpls {
+            node: ldk,
+            backup_transport,
+            lsp,
+            receiver,
+        });
     }
     #[allow(unreachable_code)]
     match config.node_config {
@@ -36,7 +51,12 @@ pub async fn build_node(
                     inner: greenlight.clone(),
                 };
                 let backup_transport = Arc::new(backup_transport);
-                Ok((greenlight, backup_transport))
+                Ok(NodeImpls {
+                    node: greenlight,
+                    backup_transport,
+                    lsp: None,
+                    receiver: None,
+                })
             }
             #[cfg(not(feature = "greenlight"))]
             Err(crate::node_api::NodeError::generic(
